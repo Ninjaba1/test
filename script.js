e6a690bb6b0e09c2b277a54f5083c319baf421d0
myrza <myrza@myrzas-MacBook-Air.local>
2024-01-03 02:30:46 +0600

    clear

--- script.js
+++
@@ -1,7 +0,0 @@
-const image = document.querySelector(".image");
-const title = document.querySelector(".title");
-
-image.addEventListener("click", () => {
-  title.classList.add("active");
-  image.classList.add("active");
-});
